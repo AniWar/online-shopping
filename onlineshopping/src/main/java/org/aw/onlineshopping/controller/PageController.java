5eee81025da8163fdb02d9392e7e82a18455f0a0
Aniket Warey <akw42@AniWar-PC>
2017-10-10 22:22:55 +0530

    Added theme

--- onlineshopping/src/main/java/org/aw/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/org/aw/onlineshopping/controller/PageController.java
@@ -1,37 +1,33 @@
 package org.aw.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 	
 	@RequestMapping(value={"/","/home","/index"})
-	public ModelAndView index(){
+	public ModelAndView viewHome(){
 		 ModelAndView mv=new ModelAndView("page");
-		 mv.addObject("greeting","Welcome to the world");
+		 mv.addObject("title", "Home");
+		 mv.addObject("userClickHome",true);
 		 return mv;
 	}
 	
-	@RequestMapping(value="query")
-	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting){
-		if(greeting==null){
-			greeting="Welcome dear";
-		}
+	@RequestMapping(value="/about")
+	public ModelAndView viewAbout(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
 	
-	@RequestMapping(value="/test/{path}")
-	public ModelAndView test2(@PathVariable(value="path",required=false)String id){
+	@RequestMapping(value="/contact")
+	public ModelAndView viewContact(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", id);
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
-		
 	}
-
 }
